a5b9d37f30c76851435177da81987bac349346e0
Vincent Lark <vincent.lark@gmail.com>
2019-10-07 08:55:22 +0200

    rebuilding site Lun  7 oct 2019 08:55:22 CEST

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t creative # if using a theme, replace with `hugo -t <YOURTHEME>`
+npm run hugo -- -t creative # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # make Things index the main index
 cp docs/things/index.html docs/index.html
